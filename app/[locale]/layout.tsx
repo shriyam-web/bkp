3ef756f379cc76ba8b8763bf73d61ec2d7feb19f
User <user@example.com>
2025-12-15 13:03:15 +0530

    \"Fix globals.css import path\"

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,4 @@
-import './../../globals.css';
+import '../globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Locale, i18n } from '@/i18n.config';
